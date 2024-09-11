27632d684f4e78438b8a293df7cd13c120758e8d
urielbrunomota <ubumo@hotmail.com>
2024-09-11 17:08:53 -0400

    B: Added circleshape.py and set clock object in main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = Clock.tick(60) / 1000
+
 
 
 
